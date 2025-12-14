db5be6047541769d9c0dd9e1296f32bfaa85bc2e
Jimby Raynor <67350304+JimbyRaynor@users.noreply.github.com>
2025-12-14 13:09:18 +1100

    Add files via upload

--- LEDcolour.cpp
+++ LEDcolour.cpp
@@ -10,6 +10,8 @@
 
 // raylib uses float for most numbers, and so use 2.0f to convert int to float. Note that 2.0 will be a double
 
+// put yellow border around black pixels (non zero)
+
 using namespace std;
 
 int screenWidth = 1200; 
@@ -44,14 +46,41 @@ Color rbdarkblue = HexToColour(0x00008B);
 Color rblightred = HexToColour(0xFF6666);
 Color rbred = HexToColour(0xFF0000);
 Color rbdarkred = HexToColour(0x8B0000);
+Color rborange = HexToColour(0xFF5900);
+Color rblightorange = HexToColour(0xFFD580);
+Color rbdarkorange = HexToColour(0xFF8C00);  
+Color rblightgreen = HexToColour(0x66C266);
+Color rbgreen = HexToColour(0x279627);  
+Color rbdarkgreen = HexToColour(0x006400);
+Color rblightpink = HexToColour(0xFFD1F0);
+Color rbpink = HexToColour(0xF498EC);  
+Color rbdarkpink = HexToColour(0xC71585);
+Color rblightyellow = HexToColour(0xFFFFE0);
+Color rbyellow = HexToColour(0xFFFF00);
+Color rbdarkyellow = HexToColour(0xB8860B);  
+Color rblightgrey = HexToColour(0xDDDDDD);
+Color rbgrey = HexToColour(0xAAAAAA);
+Color rbdarkgrey = HexToColour(0x7777777);
+Color rblightbrown = HexToColour(0xC19153);
+Color rbbrown = HexToColour(0x8B4513);
+Color rbdarkbrown = HexToColour(0x4C3A23);
+Color rblightaqua = HexToColour(0xE0FFFF);
+Color rbaqua = HexToColour(0x00FFFF);
+Color rbdarkaqua = HexToColour(0x008B8B);
+Color rblightpurple = HexToColour(0xE6E6FA);
+Color rbpurple = HexToColour(0xBE1CBE);
+Color rbdarkpurple = HexToColour(0x4B0082);
 
-Color AllColours[60] = {rblightblue, rbblue, rbdarkblue, rblightred, rbred, rbdarkred};
+Color AllColours[60] = {rblightblue, rbblue, rbdarkblue, rblightred, rbred, rbdarkred, rblightorange, rborange, rbdarkorange,
+                       rblightgreen, rbgreen, rbdarkgreen, rblightpink, rbpink, rbdarkpink, rblightyellow, rbyellow, rbdarkyellow,
+                       rblightgrey, rbgrey, rbdarkgrey, rblightbrown, rbbrown, rbdarkbrown, rblightaqua, rbaqua, rbdarkaqua,
+                       rblightpurple, rbpurple, rbdarkpurple};
 
 int selectedcolourindex = 1;
 
 Color getColour(int myindex)
 {
-if ((myindex >= 1) and (myindex <= 6) )
+if ((myindex >= 1) and (myindex <= 30) )
    return AllColours[myindex-1];
 else
    return rbblack; 
@@ -74,13 +103,9 @@ void DrawGrid()
 
 void DrawPalette()
 {
-    DrawRectangle(palettex,palettey,40,40,rblightblue);
-    DrawRectangle(palettex+40,palettey,40,40,rbblue);
-    DrawRectangle(palettex+80,palettey,40,40,rbdarkblue);
-
-    DrawRectangle(palettex,palettey+40,40,40,rblightred);
-    DrawRectangle(palettex+40,palettey+40,40,40,rbred);
-    DrawRectangle(palettex+80,palettey+40,40,40,rbdarkred);
+    for (int i=0;i < 10; i++)
+      for (int j=0; j < 3; j++) 
+    DrawRectangle(palettex+j*40,palettey+i*40,40,40,getColour(i*3+j+1));
 
 }
 
